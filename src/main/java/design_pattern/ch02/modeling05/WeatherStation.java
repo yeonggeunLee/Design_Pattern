777ac6fe0e5e85f0e42ecbfd33a7f403e9885885
이영근 <dldudrms506@gmail.com>
2023-03-30 22:20:21 +0900

    ch02_modeling05 Complete

--- src/main/java/design_pattern/ch02/modeling05/WeatherStation.java
+++ src/main/java/design_pattern/ch02/modeling05/WeatherStation.java
@@ -12,6 +12,8 @@ public class WeatherStation {
         weatherData.subscribe(forecastDisplay);
 
         // 결과가 위에서 subscribe 시킨 순서와 다르게 순서가 바뀌어서 동작한다
+        // 같은 쓰레드라도 다르게 동작한다.
+        // Flow api를 사용시 앞의 다른 모델들과는 다르다. 이것은 비동기적이기 때문이다.
         weatherData.setMeasurements(80, 65, 30.4f);
         System.out.println("------------------------------------------");
         weatherData.setMeasurements(82, 70, 29.2f);
